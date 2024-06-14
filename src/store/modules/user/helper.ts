e72c374d9e0d4d75d8945fa581006cd64678e22c
yenkj <49618620+yenkj@users.noreply.github.com>
2024-06-14 19:45:43 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
+      avatar: 'https://raw.githubusercontent.com/yenkj/chatgpt-web/main/src/assets/avatar.jpg',
       name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      description: 'Star on <a href="https://github.com/yenkj/ChatGPT-Web" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
